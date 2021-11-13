ea3d8ffb24aa3eb08769d337cd900db4a2f9df65
Tim Ledbetter <timledbetter@gmail.com>
2021-11-13 11:49:21 +0000

    Removed unused implementation of InMemoryQueue<TData, TPriority>

--- DistributedWebCrawler.Core/Queue/InMemoryQueue.cs
+++ DistributedWebCrawler.Core/Queue/InMemoryQueue.cs
@@ -6,30 +6,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace DistributedWebCrawler.Core.Queue
 {
-    public class InMemoryQueue<TData, TPriority> : IProducerConsumer<TData, TPriority>
-        where TData : notnull
-        where TPriority : notnull
-    {
-        private readonly SimplePriorityQueue<TData, TPriority> _priorityQueue;
-
-        public InMemoryQueue(IComparer<TPriority> priorityComparer)
-        {
-            _priorityQueue = new SimplePriorityQueue<TData, TPriority>(priorityComparer);
-        }
-
-        public int Count => _priorityQueue.Count;
-
-        public void Enqueue(TData data, TPriority priority)
-        {
-            _priorityQueue.Enqueue(data, priority);
-        }
-
-        public bool TryDequeue([NotNullWhen(returnValue: true)] out TData? data)
-        {
-            return _priorityQueue.TryDequeue(out data);
-        }
-    }
-
     public class InMemoryQueue<TData> : IProducerConsumer<TData>
     {
         private readonly ConcurrentQueue<TData> _queue;
