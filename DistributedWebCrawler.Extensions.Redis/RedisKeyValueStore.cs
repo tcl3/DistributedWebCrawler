b14b7953a7ed4532cecd964c67abdfc8d843b149
Tim Ledbetter <timledbetter@gmail.com>
2021-11-26 22:47:17 +0000

    Added missing Extensions.Redis files and fixed broken paths

---
+++ DistributedWebCrawler.Extensions.Redis/RedisKeyValueStore.cs
@@ -0,0 +1,43 @@
+﻿using DistributedWebCrawler.Core.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DistributedWebCrawler.Extensions.Redis
+{
+    public class RedisKeyValueStore : IKeyValueStore
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisKeyValueStore(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public Task PutAsync(string key, string value, CancellationToken cancellationToken, TimeSpan? expireAfter)
+        {
+            if (expireAfter == null)
+            {
+                return _cache.SetStringAsync(key, value, cancellationToken);
+            }
+            else
+            {
+                var options = new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = expireAfter
+                };
+
+                return _cache.SetStringAsync(key, value, options, cancellationToken);
+            }
+        }
+
+        public Task<string?> GetAsync(string key, CancellationToken cancellationToken)
+        {
+            
+            return _cache.GetStringAsync(key, cancellationToken);
+        }
+
+        public Task RemoveAsync(string key, CancellationToken cancellationToken)
+        {
+            return _cache.RemoveAsync(key, cancellationToken);
+        }
+    }
+}
---
+++ DistributedWebCrawler.Extensions.Redis/ServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using DistributedWebCrawler.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System.Collections.Concurrent;
+
+namespace DistributedWebCrawler.Extensions.Redis
+{
+    public static class ServiceCollectionExtensions
+    {
+        private static ConcurrentDictionary<IConfiguration, IConnectionMultiplexer> _connectionMultiplexerLookup = new();
+        public static IServiceCollection AddRedisKeyValueStore(this IServiceCollection services, IConfiguration redisConfiguration)
+        {
+            services.AddRedisCache(redisConfiguration);
+            services.AddSingleton<IKeyValueStore, RedisKeyValueStore>();
+            return services;
+        }
+
+        private static IServiceCollection AddRedisConnection(this IServiceCollection services, IConfiguration redisConfiguration)
+        {
+            services.AddSingleton<IConnectionMultiplexer>(s => GetConnectionMultiplexer(redisConfiguration));
+
+            return services;
+        }
+
+        public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration redisConfiguration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.ConnectionMultiplexerFactory = () => Task.FromResult(GetConnectionMultiplexer(redisConfiguration));
+            });
+
+            return services;
+        }
+
+        private static IConnectionMultiplexer GetConnectionMultiplexer(IConfiguration redisConfiguration)
+        {
+            return _connectionMultiplexerLookup.GetOrAdd(redisConfiguration, CreateConnectionMultiplexer);
+        }
+
+        private static IConnectionMultiplexer CreateConnectionMultiplexer(IConfiguration redisConfiguration)
+        {
+            var connectionString = redisConfiguration.GetValue<string>("REDIS_CONNECTIONSTRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Variable 'REDIS_CONNECTIONSTRING' not set");
+            }
+            return ConnectionMultiplexer.Connect(connectionString);
+        }
+    }
+}
