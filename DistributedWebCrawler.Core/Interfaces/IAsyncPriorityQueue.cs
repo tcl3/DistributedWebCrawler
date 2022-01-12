1eeacc3f9963187bc1922712625433e5c246f49c
Tim Ledbetter <timledbetter@gmail.com>
2022-01-12 19:09:40 +0000

    Update IAsyncPriorityQueueInterface to ensure passing the CancellationToken is not mandatory.

--- DistributedWebCrawler.Core/Interfaces/IAsyncPriorityQueue.cs
+++ DistributedWebCrawler.Core/Interfaces/IAsyncPriorityQueue.cs
@@ -5,7 +5,7 @@ namespace DistributedWebCrawler.Core.Interfaces
 {
     public interface IAsyncPriorityQueue<TData, TPriority> 
     {
-        Task<bool> EnqueueAsync(TData item, TPriority priority, CancellationToken cancellationToken);
-        Task<TData> DequeueAsync(CancellationToken cancellationToken);
+        Task<bool> EnqueueAsync(TData item, TPriority priority, CancellationToken cancellationToken = default);
+        Task<TData> DequeueAsync(CancellationToken cancellationToken = default);
     }
 }
