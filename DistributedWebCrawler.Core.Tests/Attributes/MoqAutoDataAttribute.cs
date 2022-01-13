170449e2269cd2e70d8c75e4a4ea299f2ff5a18a
Tim Ledbetter <timledbetter@gmail.com>
2022-01-13 23:21:12 +0000

    Added AutoFixture.AutoMoq and dependencies to the Core.Tests project for easier mocking.

---
+++ DistributedWebCrawler.Core.Tests/Attributes/MoqAutoDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace DistributedWebCrawler.Core.Tests.Attributes
+{
+    internal class MoqAutoDataAttribute : AutoDataAttribute
+    {
+        public MoqAutoDataAttribute() : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+
+        }
+    }
+}
