755c5f99b81d43482fa98a7c9acd10f2fc682db8
Tim Ledbetter <timledbetter@gmail.com>
2022-01-13 17:56:31 +0000

    Added unit tests for ComponentDescriptor.

---
+++ DistributedWebCrawler.Core.Tests/ComponentDescriptorTests.cs
@@ -0,0 +1,21 @@
+﻿using DistributedWebCrawler.Core.Tests.Fakes;
+using System.Linq;
+using Xunit;
+
+namespace DistributedWebCrawler.Core.Tests
+{
+    public class ComponentDescriptorTests
+    {
+        [Fact]
+        public void FromAssembliesShouldReturnAllComponentsInAssembly()
+        {
+            var expectedComponentTypes = new[] { typeof(TestComponent), typeof(TestComponentWithNameAttribute) };
+            var componentDescriptors = ComponentDescriptor.FromAssemblies(new[] { typeof(TestComponentMarkerInterface).Assembly }).ToList();
+
+            Assert.Equal(expectedComponentTypes.Length, componentDescriptors.Count);
+
+            var expectedComponentTypeNames = expectedComponentTypes.Select(x => x.FullName).OrderBy(x => x);
+            Assert.Equal(expectedComponentTypeNames, componentDescriptors.Select(x => x.ComponentType.FullName).OrderBy(x => x));
+        }
+    }
+}
