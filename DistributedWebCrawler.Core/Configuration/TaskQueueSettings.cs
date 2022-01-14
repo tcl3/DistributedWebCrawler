e93e3f3fe372fd6edd07ec68202f6133560d2b50
Tim Ledbetter <timledbetter@gmail.com>
2022-01-14 08:10:21 +0000

    Lowered maximum allowed value of TaskQueueSettings.QueueItemTimeoutSeconds to 1 day as CancellationTokenSource.CancelAfter() threw an exception when it was set to int.MaxValue.

--- DistributedWebCrawler.Core/Configuration/TaskQueueSettings.cs
+++ DistributedWebCrawler.Core/Configuration/TaskQueueSettings.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using DistributedWebCrawler.Core.Constants;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DistributedWebCrawler.Core.Configuration
 {
     public abstract class TaskQueueSettings
     {
-        [Range(1, int.MaxValue, ErrorMessage = nameof(QueueItemTimeoutSeconds) + " must be a positive integer")]
+        [Range(1, 1 * TimeConstants.SecondsPerDay)]
         public int QueueItemTimeoutSeconds { get; init; }
 
         internal abstract int MaxConcurrentItems { get; }
---
+++ DistributedWebCrawler.Core/Constants/TimeConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace DistributedWebCrawler.Core.Constants
+{
+    public static class TimeConstants
+    {
+        public const int SecondsPerMinute = 60;
+        public const int SecondsPerHour = SecondsPerMinute * 60;
+        public const int SecondsPerDay = SecondsPerHour * 24;
+        public const int SecondsPerWeek = SecondsPerDay * 7;
+
+        public const int MillisecondsPerMinute = 60000;
+        public const int MillisecondsPerHour = MillisecondsPerMinute * 60;
+        public const int MillisecondsPerDay = MillisecondsPerHour * 24;
+        public const int MillisecondsPerWeek = MillisecondsPerDay * 7;
+    }
+}
