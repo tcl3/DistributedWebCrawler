601a497bcd5ad3524fa6930b1e8ada571d00d673
Tim Ledbetter <timledbetter@gmail.com>
2022-01-20 00:07:37 +0000

    Added unit tests for CompositeSeeder.

---
+++ DistributedWebCrawler.Core.Tests/CompositeSeederTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture.Xunit2;
+using DistributedWebCrawler.Core.Interfaces;
+using DistributedWebCrawler.Core.Seeding;
+using DistributedWebCrawler.Core.Tests.Attributes;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DistributedWebCrawler.Core.Tests
+{
+    public class CompositeSeederTests
+    {
+        [Theory]
+        [MoqAutoData]
+        public async Task EnsureAllUnderlyingSeederComponentsAreCalled(
+            [Frozen] IEnumerable<ISeederComponent> seederComponents,
+            CompositeSeeder sut)
+        {
+            await sut.SeedAsync();
+
+            foreach (var seederComponent in seederComponents)
+            {
+                var seederComponentMock = Mock.Get(seederComponent);
+                seederComponentMock.Verify(x => x.SeedAsync(), Times.Once());
+            }
+        }
+    }
+}
