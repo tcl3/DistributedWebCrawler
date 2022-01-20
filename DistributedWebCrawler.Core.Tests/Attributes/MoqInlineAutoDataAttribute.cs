4cac911791851e8b7acedc6253dd1d7c2b81e7de
Tim Ledbetter <timledbetter@gmail.com>
2022-01-20 07:24:27 +0000

    Added MoqInlineAutoDataAttribute, to combine auto mocking with inline data.

---
+++ DistributedWebCrawler.Core.Tests/Attributes/MoqInlineAutoDataAttribute.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+
+namespace DistributedWebCrawler.Core.Tests.Attributes
+{
+    internal class MoqInlineAutoDataAttribute : InlineAutoDataAttribute
+    {
+        public MoqInlineAutoDataAttribute(
+            ICustomization[] additionalCustomizations,
+            bool configureMembers = false,
+            params object?[] values)
+            : base(new MoqAutoDataAttribute(additionalCustomizations, configureMembers), values)
+        {
+
+        }
+
+        public MoqInlineAutoDataAttribute(
+            ICustomization additionalCustomization,
+            bool configureMembers = false,
+            params object?[] values)
+            : base(new MoqAutoDataAttribute(additionalCustomization, configureMembers), values)
+        {
+
+        }
+
+        public MoqInlineAutoDataAttribute(bool configureMembers = false, params object?[] values)
+            : base(new MoqAutoDataAttribute(configureMembers), values)
+        {
+
+        }
+    }
+}
