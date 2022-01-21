85cfa08ebb4e681c2c361081d805e1ae4a30cf99
Tim Ledbetter <timledbetter@gmail.com>
2022-01-21 23:48:02 +0000

    Added RobotsCacheReader tests to cover different kinds robots.txt content and refactored to avoid repeated test code.

--- DistributedWebCrawler.Core.Tests/RobotsCacheReaderTests.cs
+++ DistributedWebCrawler.Core.Tests/RobotsCacheReaderTests.cs
@@ -4,6 +4,7 @@ using DistributedWebCrawler.Core.Robots;
 using DistributedWebCrawler.Core.Tests.Attributes;
 using Moq;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -12,38 +13,115 @@ namespace DistributedWebCrawler.Core.Tests
 {
     public class RobotsCacheReaderTests
     {
-        private static readonly Uri MockUri = new Uri("http://mock.test/");
         private const string MockRobotsTxtContent = "MockRobotsTxtContent";
 
-        private readonly CancellationTokenSource _cts = new(TimeSpan.FromSeconds(1));
-
         [Theory]
-        [MoqAutoData]
-        public async Task GetRobotsTextShouldReturnFalseWhenUriNotCached([Frozen] Mock<IKeyValueStore> keyValueStoreMock, RobotsCacheReader sut)
+        [RobotsAutodata(robotsContent: null, expectedResult: false)]
+        public async Task GetRobotsTextShouldReturnFalseWhenUriNotCached(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock, 
+            RobotsCacheReader sut)
         {
-            keyValueStoreMock.Setup(x => x.GetAsync(It.IsAny<string>()))
-                .ReturnsAsync(() => null);
-
-            var ifExistsActionCalled = false;
-            var result = await sut.GetRobotsTxtAsync(MockUri, robots => ifExistsActionCalled = true, _cts.Token);
-            Assert.False(result);
-            Assert.False(ifExistsActionCalled);
-
-            keyValueStoreMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once());
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut);
         }
 
         [Theory]
-        [MoqAutoData]
-        public async Task GetRobotsTextShouldReturnTrueWhenUriIsCached([Frozen] Mock<IKeyValueStore> keyValueStoreMock, RobotsCacheReader sut)
+        [RobotsAutodata(robotsContent: MockRobotsTxtContent, expectedResult: true)]
+        public async Task GetRobotsTextShouldReturnTrueWhenUriIsCached(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock, 
+            RobotsCacheReader sut)
+        {
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut);
+        }
+
+        [Theory]
+        [RobotsAutodata(robotsContent: "Allow: /", expectedResult: true)]
+        public async Task AllowedShouldReturnTrueWhenUriIsAllowedInRobotsTxt(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock, 
+            RobotsCacheReader sut)
+        {
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut, robots =>
+            {
+                Assert.True(robots.Allowed(uri));
+            });
+        }
+
+        [Theory]
+        [RobotsAutodata(robotsContent: "User-agent: *\nDisallow: /", expectedResult: true)]
+        public async Task AllowedShouldReturnFalseWhenUriIsDisallowedInRobotsTxt(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock,
+            RobotsCacheReader sut)
+        {
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut, robots =>
+            {
+                Assert.False(robots.Allowed(new Uri("/", UriKind.Relative)));
+            });
+        }
+
+        [Theory]
+        [RobotsAutodata(robotsContent: "User-agent: *\nCrawl-Delay: 10", expectedResult: true)]
+        public async Task EnsureCrawlDelayPropertyIsPopulatedWhenRobotsTxtContainsCrawlDelay(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock,
+            RobotsCacheReader sut)
+        {
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut, robots =>
+            {
+                Assert.Equal(10, robots.CrawlDelay);
+            });
+        }
+
+        [Theory]
+        [RobotsAutodata(robotsContent: "User-agent: *\nSitemap: http://test.com/sitemap.xml", expectedResult: true)]
+        public async Task EnsureSitemapUrlsPropertyIsPopulatedWhenRobotsTxtContainsSitemaps(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock,
+            RobotsCacheReader sut)
+        {
+            await RobotsTxtContentTest(robotsContent, expectedResult, uri, keyValueStoreMock, sut, robots =>
+            {
+                Assert.Single(robots.SitemapUrls);
+                Assert.Equal("http://test.com/sitemap.xml", robots.SitemapUrls.First());
+            });            
+        }
+
+        private async Task RobotsTxtContentTest(
+            string robotsContent,
+            bool expectedResult,
+            Uri uri,
+            Mock<IKeyValueStore> keyValueStoreMock,
+            RobotsCacheReader sut,
+            Action<IRobots>? additionalRobotsAssertions = null)
         {
             keyValueStoreMock.Setup(x => x.GetAsync(It.IsAny<string>()))
-                .ReturnsAsync(() => MockRobotsTxtContent);
+               .ReturnsAsync(() => robotsContent);
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
 
             var ifExistsActionCalled = false;
-            var result = await sut.GetRobotsTxtAsync(MockUri, robots => ifExistsActionCalled = true, _cts.Token);
-            Assert.True(result);
-            Assert.True(ifExistsActionCalled);
+            var result = await sut.GetRobotsTxtAsync(uri, robots =>
+            {
+                ifExistsActionCalled = true;
+                additionalRobotsAssertions?.Invoke(robots);
+            }, cts.Token);
 
+            Assert.Equal(expectedResult, result); ;
+            Assert.Equal(expectedResult, ifExistsActionCalled);
 
             keyValueStoreMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once());
         }
