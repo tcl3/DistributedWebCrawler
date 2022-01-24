f95cae96bb05fb170d1ac77e697bd38a8dae93d9
Tim Ledbetter <timledbetter@gmail.com>
2022-01-24 18:12:03 +0000

    Added missing test attribute

---
+++ DistributedWebCrawler.Core.Tests/Attributes/RobotsAutodataAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace DistributedWebCrawler.Core.Tests.Attributes
+{
+    internal class RobotsAutodataAttribute : MoqInlineAutoDataAttribute
+    {
+        public RobotsAutodataAttribute(string? robotsContent, bool expectedResult)
+            : base(values: new object?[] { robotsContent, expectedResult})
+        {
+
+        }
+    }
+}
