13e4096419e3306d0c883555f9b6fcf2b2fd6aa9
Tim Ledbetter <timledbetter@gmail.com>
2022-01-25 21:50:15 +0000

    Added EventReceiverCollection unit tests.

---
+++ DistributedWebCrawler.Core.Tests/EventReceiverCollectionTests.cs
@@ -0,0 +1,78 @@
+﻿using AutoFixture.Xunit2;
+using DistributedWebCrawler.Core.Interfaces;
+using DistributedWebCrawler.Core.Model;
+using DistributedWebCrawler.Core.Tests.Attributes;
+using DistributedWebCrawler.Core.Tests.Fakes;
+using Moq;
+using Xunit;
+
+namespace DistributedWebCrawler.Core.Tests
+{
+    public class EventReceiverCollectionTests
+    {
+        [Theory]
+        [MoqAutoData]
+        public void SchedulerPropertyShouldCallCorrectEventReceiver(
+            [Frozen] Mock<IEventReceiverFactory> eventReceiverFactoryMock,
+            EventReceiverCollection sut)
+        {
+            var result = sut.Scheduler;
+            
+            Assert.NotNull(result);
+            eventReceiverFactoryMock.Verify(x => 
+                x.Get<SchedulerSuccess, ErrorCode<SchedulerFailure>>(), Times.Once());
+        }
+
+        [Theory]
+        [MoqAutoData]
+        public void IngesterPropertyShouldCallCorrectEventReceiver(
+            [Frozen] Mock<IEventReceiverFactory> eventReceiverFactoryMock,
+            EventReceiverCollection sut)
+        {
+            var result = sut.Ingester;
+
+            Assert.NotNull(result);
+            eventReceiverFactoryMock.Verify(x =>
+                x.Get<IngestSuccess, IngestFailure>(), Times.Once());
+        }
+
+        [Theory]
+        [MoqAutoData]
+        public void ParserPropertyShouldCallCorrectEventReceiver(
+            [Frozen] Mock<IEventReceiverFactory> eventReceiverFactoryMock,
+            EventReceiverCollection sut)
+        {
+            var result = sut.Parser;
+
+            Assert.NotNull(result);
+            eventReceiverFactoryMock.Verify(x =>
+                x.Get<ParseSuccess, ErrorCode<ParseFailure>>(), Times.Once());
+        }
+
+        [Theory]
+        [MoqAutoData]
+        public void RobotsDownloaderPropertyShouldCallCorrectEventReceiver(
+            [Frozen] Mock<IEventReceiverFactory> eventReceiverFactoryMock,
+            EventReceiverCollection sut)
+        {
+            var result = sut.RobotsDownloader;
+
+            Assert.NotNull(result);
+            eventReceiverFactoryMock.Verify(x =>
+                x.Get<RobotsDownloaderSuccess, ErrorCode<RobotsDownloaderFailure>>(), Times.Once());
+        }
+
+        [Theory]
+        [MoqAutoData]
+        public void OfTypeShouldCallCorrectEventReceiver(
+            [Frozen] Mock<IEventReceiverFactory> eventReceiverFactoryMock,
+            EventReceiverCollection sut)
+        {
+            var result = sut.OfType<TestSuccess, ErrorCode<TestFailure>>();
+
+            Assert.NotNull(result);
+            eventReceiverFactoryMock.Verify(x =>
+                x.Get<TestSuccess, ErrorCode<TestFailure>>(), Times.Once());
+        }        
+    }
+}
