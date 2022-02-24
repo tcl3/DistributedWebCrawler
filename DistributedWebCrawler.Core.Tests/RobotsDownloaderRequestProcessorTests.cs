99b3f340d0e0b93bb7f89d83848a004f646b9ebe
Tim Ledbetter <timledbetter@gmail.com>
2022-02-24 17:19:38 +0000

    Added more assertions to RobotsDownloaderRequestProcessor tests to cover cases found by Stryker.

--- DistributedWebCrawler.Core.Tests/RobotsDownloaderRequestProcessorTests.cs
+++ DistributedWebCrawler.Core.Tests/RobotsDownloaderRequestProcessorTests.cs
@@ -18,18 +18,23 @@ namespace DistributedWebCrawler.Core.Tests
         [Theory]
         [MoqAutoData(configureMembers: true)]
         public async Task ProcessItemShouldReturnSuccess(
-            [Frozen] Mock<IKeyValueStore> keyValueStore,
+            [Frozen] Mock<IKeyValueStore> keyValueStoreMock,
+            [Frozen] Mock<IProducer<SchedulerRequest>> schedulerRequestProducerMock,
             [Frozen] SchedulerRequest schedulerRequest,
             [Frozen] RobotsRequest request, 
             RobotsDownloaderRequestProcessor sut)
         {
             // FIXME: This was added because Moq cannot construct SchedulerRequest by itself
             // Surely setup like this can be automated though?
-            keyValueStore.Setup(x => x.GetAsync<SchedulerRequest>(It.IsAny<string>()))
+            keyValueStoreMock.Setup(x => x.GetAsync<SchedulerRequest>(It.IsAny<string>()))
                 .ReturnsAsync(schedulerRequest);
 
             var result = await sut.ProcessItemAsync(request);
+
             Assert.IsAssignableFrom<QueuedItemResult<RobotsDownloaderSuccess>>(result);
+            keyValueStoreMock.Verify(x => x.GetAsync<SchedulerRequest>(It.IsAny<string>()), Times.Once());
+            keyValueStoreMock.Verify(x => x.RemoveAsync(It.IsAny<string>()), Times.Once());
+            schedulerRequestProducerMock.Verify(x => x.Enqueue(schedulerRequest), Times.Once());
         }
     }
 }
